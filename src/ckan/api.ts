1890f12be86823154d6e72054c251bf830bcf668
Chas Nelson <github@chasnelson.co.uk>
2024-08-23 14:39:22 +0100

    feat: make the system work without facets (a bit hacky)

--- src/ckan/api.ts
+++ src/ckan/api.ts
@@ -28,40 +28,52 @@ export const fetchCountries = (config: ConfigType) =>
             records.map((record: { [key: string]: any }) => record[config.API_FIELDS.country]),
         );
 
-export const fetchCountryData = (config: ConfigType, country: string) =>
-    axios
+export const fetchCountryData = (config: ConfigType, country: string) => {
+    let fields = [config.API_FIELDS.country, config.API_FIELDS.time_axis, config.API_FIELDS.value].join(',');
+    if (config.API_FIELDS.segment && !(config.API_FIELDS.segment === '')) {
+        fields = [
+            config.API_FIELDS.country,
+            config.API_FIELDS.time_axis,
+            config.API_FIELDS.segment,
+            config.API_FIELDS.value,
+        ].join(',');
+    }
+
+    return axios
         .get(config.DATASTORE_PATH, {
             params: {
                 resource_id: config.RESOURCE_ID,
-                fields: [
-                    config.API_FIELDS.country,
-                    config.API_FIELDS.time_axis,
-                    config.API_FIELDS.segment,
-                    config.API_FIELDS.value,
-                ].join(','),
+                fields: fields,
                 q: JSON.stringify({
                     [config.API_FIELDS.country]: country,
                 }),
                 limit: '32000',
             },
         })
-        .then(({ data: records }: AxiosResponse) =>
-            records.map((record: { [key: string]: any }) => ({
+        .then(({ data: records }: AxiosResponse) => {
+            return records.map((record: { [key: string]: any }) => ({
                 year: record[config.API_FIELDS.time_axis],
-                sex: record[config.API_FIELDS.segment],
+                sex: record[config.API_FIELDS.segment] || config.SEXES[0].id,
                 value: formatValue(config, record[config.API_FIELDS.value]),
-            })),
-        );
+            }))
+        });
+};
 
-export const fetchYearData = (config: ConfigType, year: number, sex: string) =>
-    axios
+export const fetchYearData = (config: ConfigType, year: number, sex: string) => {
+    let query = JSON.stringify({
+        [config.API_FIELDS.time_axis]: year.toString(),
+    });
+    if (config.API_FIELDS.segment && !(config.API_FIELDS.segment === '') && sex && !(sex === '')) {
+        query = JSON.stringify({
+            [config.API_FIELDS.time_axis]: year.toString(),
+            [config.API_FIELDS.segment]: sex,
+        });
+    }
+    return axios
         .get(config.DATASTORE_PATH, {
             params: {
                 resource_id: config.RESOURCE_ID,
-                q: JSON.stringify({
-                    [config.API_FIELDS.time_axis]: year.toString(),
-                    [config.API_FIELDS.segment]: sex,
-                }),
+                q: query,
                 fields: [config.API_FIELDS.country, config.API_FIELDS.value].join(','),
                 limit: '32000',
             },
@@ -74,3 +86,4 @@ export const fetchYearData = (config: ConfigType, year: number, sex: string) =>
                 }))
                 .sort((a, b) => a.value - b.value),
         );
+};
